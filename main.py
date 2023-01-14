eec9d1f0652b6510b2989dbb5c5e272333c1d5fc
kano <steen4145@gmail.com>
2023-01-14 01:34:41 +0100

    section 4/project completed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from PIL import Image
-im = Image.open('ascii-pineapple.jpg')
+#insert file name in this string
+picture_name = 'typing-man-typing.gif'
+im = Image.open(picture_name)
+
 
 print('Successfully loaded image!')
 
@@ -9,27 +12,45 @@ r, g, b = rgb_im.getpixel((1, 1))
 hgt = im.height
 wdh = im.width
 
-pixel_matrix = [[0 for y in range(hgt)] for x in range(wdh)] 
+pixel_matrix = [[0 for x in range(wdh)] for y in range(hgt)] 
 
-for h in range(hgt):
-    for w in range(wdh):
+for w in range(wdh):
+    for h in range(hgt):
         r, g, b = rgb_im.getpixel((w, h))
-        pixel_matrix[w][h] = (r, g, b)
+        pixel_matrix[h][w] = (r, g, b)
 
 #print(pixel_matrix)
 
 print('Pixel matrix size:', len(pixel_matrix), 'x', len(pixel_matrix[0]))
-print('Iterating through pixel contents:')
 
-brightness_matrix = [[0 for y in range(hgt)] for x in range(wdh)] 
+brightness_matrix = [[0 for x in range(wdh)] for y in range(hgt)] 
 
-for h in range(hgt):
-    for w in range(wdh):
-        r, g, b = pixel_matrix[w][h]
+for w in range(wdh):
+    for h in range(hgt):
+        r, g, b = pixel_matrix[h][w]
         brightness = (r + g + b) / 3
-        pixel_matrix[w][h] = round(brightness)
+        pixel_matrix[h][w] = round(brightness)
+
+#64 chars
+raw_chars = r'`^",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
+
+for w in range(wdh):
+    for h in range(hgt):
+        brightness = pixel_matrix[h][w]
+        ratio = 256 / len(raw_chars)
+        index = brightness / ratio
+        
+        pixel_matrix[h][w] = raw_chars[int(index)]
+
+output = ''
 
 for x in range(len(pixel_matrix)):
+    output += '\n'
     for y in range(len(pixel_matrix[x])):
         pixel = pixel_matrix[x][y]
-        print(pixel)
+        output += pixel
+              
+open(picture_name + '.txt', 'x')
+with open(picture_name + '.txt', 'w') as text_file:
+    text_file.write(output)
